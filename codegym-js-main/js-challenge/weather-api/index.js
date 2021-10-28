b9935f2690e5f0d628f33e99dfbf1edbdff98373
Taiyo Aragane <t.m.ny.0404@gmail.com>
2021-10-28 11:11:08 +0900

    1回目プルリクエスト

--- codegym-js-main/js-challenge/weather-api/index.js
+++ codegym-js-main/js-challenge/weather-api/index.js
@@ -1,20 +1,20 @@
 const select = document.getElementById("selection");//タブの情報を取得
 let targetCityName = "london";
 
+//タブで選択された都市の天気の情報を取ってくる
 const serch = () => {
     const appId = "4b5774e9f3d2a07b84f0f2f88e486224";
     const requestUrl = "https://api.openweathermap.org/data/2.5/weather?APPID=" + appId + "&lang=ja&units=metric&q=" + targetCityName + ",jp;";
     const xmlhttp = new XMLHttpRequest();
 
-    xmlhttp.onreadystatechange = function () {
+    xmlhttp.onreadystatechange = () => {
         //readyState 4: リクエスト終了
-        if (this.readyState == 4) {
-            let data = this.response;
+        if (xmlhttp.readyState == 4) {
+            const data = xmlhttp.response;
             // 場所
             document.getElementById('place').innerHTML = data.name;
             // 天気
             document.getElementById('weather').innerHTML = data.weather[0].main;
-
             // 気温
             document.getElementById('temp').innerHTML = data.main.temp;
 
@@ -27,7 +27,8 @@ const serch = () => {
     //通信を実行する
     xmlhttp.send();
 };
-serch();//初期のロンドンを表示
+//初期のロンドンを表示
+serch();
 
 //タブで選ばれたものを表示
 select.onchange = () => {
@@ -59,7 +60,3 @@ select.onchange = () => {
 
     };
 };
-
-
-
-
