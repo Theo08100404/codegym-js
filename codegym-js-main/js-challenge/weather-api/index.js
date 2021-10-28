a693d8d2ac5065a40a3e5f6026d021489d97c8e8
Taiyo Aragane <t.m.ny.0404@gmail.com>
2021-10-28 14:26:22 +0900

    コメント分修正

--- codegym-js-main/js-challenge/weather-api/index.js
+++ codegym-js-main/js-challenge/weather-api/index.js
@@ -1,15 +1,13 @@
 const select = document.getElementById("selection");//タブの情報を取得
-let targetCityName = "london";
-
 //タブで選択された都市の天気の情報を取ってくる
-const serch = () => {
+const serch = (targetCityName) => {
     const appId = "4b5774e9f3d2a07b84f0f2f88e486224";
     const requestUrl = "https://api.openweathermap.org/data/2.5/weather?APPID=" + appId + "&lang=ja&units=metric&q=" + targetCityName + ",jp;";
     const xmlhttp = new XMLHttpRequest();
 
     xmlhttp.onreadystatechange = () => {
         //readyState 4: リクエスト終了
-        if (xmlhttp.readyState == 4) {
+        if (xmlhttp.readyState === 4) {
             const data = xmlhttp.response;
             // 場所
             document.getElementById('place').innerHTML = data.name;
@@ -28,35 +26,7 @@ const serch = () => {
     xmlhttp.send();
 };
 //初期のロンドンを表示
-serch();
-
+serch("london");
 //タブで選ばれたものを表示
-select.onchange = () => {
-    switch (selection.value) {
-        case "0":
-            targetCityName = "london";
-            serch();
-            break;
+select.onchange = () => serch(select.value);
 
-        case "1":
-            targetCityName = "tokyo";
-            serch();
-            break;
-
-        case "2":
-            targetCityName = "paris";
-            serch();
-            break;
-
-        case "3":
-            targetCityName = "new york city";
-            serch();
-            break;
-
-        case "4":
-            targetCityName = "jakarta";
-            serch();
-            break;
-
-    };
-};
